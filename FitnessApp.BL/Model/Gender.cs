39f3b6d07b2e38e181837789a6b6934bb9b77100
Dmitriy Klimachev <klimdv@rubicon-it.ru>
2019-12-09 22:53:20 +0300

    New classes

---
+++ FitnessApp.BL/Model/Gender.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FitnessApp.BL.Model
+{
+    /// <summary>
+    /// Пол
+    /// </summary>
+    public class Gender
+    {
+        /// <summary>
+        /// Название пола
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Создать пол
+        /// </summary>
+        /// <param name="name"></param>
+        public Gender(string name)
+        {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ FitnessApp.BL/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FitnessApp.BL.Model
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    class User
+    {
+        /// <summary>
+        /// Имя пользователя
+        /// </summary>
+        public string Name { get; }
+
+        /// <summary>
+        /// Пол пользователя
+        /// </summary>
+        public Gender Gender { get; }
+
+
+    }
+}
